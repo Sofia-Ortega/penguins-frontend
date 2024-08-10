d0d1cc887d4a6a5be2ecdc23dde7825f7e602448
Sofia Ortega <s.ortega@tamu.edu>
2024-08-10 11:10:13 -0500

    Getting penguins from backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,11 @@ import Penguin from "./components/Penguin";
 import Penguins from "./components/Penguins";
 
 function App() {
-    return (
-        <div>
-            <Penguins />
-            <div>
-                <Penguin
-                    penguin={{
-                        id: 1,
-                        name: "Pablo",
-                        species: "Emperor Penguin",
-                        hunger: 10,
-                        love: 10,
-                    }}
-                />
-                <Penguin
-                    penguin={{
-                        id: 1,
-                        name: "Joe",
-                        species: "Emperor Penguin",
-                        hunger: 10,
-                        love: 10,
-                    }}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Penguins />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Penguins.tsx
+++ src/components/Penguins.tsx
@@ -1,17 +1,29 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { IPenguin } from "../types/types";
+import Penguin from "./Penguin";
 
 export default function Penguins() {
-    useEffect(() => {
-        axios
-            .get("http://localhost:8080/penguins")
-            .then((response) => {
-                console.log(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+  const [penguins, setPenguins] = useState<IPenguin[] | null>(null);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/penguins")
+      .then((response) => {
+        console.log(response.data);
+        setPenguins([...response.data]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    return <div>Penguins</div>;
+  if (penguins == null) return <div></div>;
+
+  return (
+    <div>
+      {penguins.map((penguin) => (
+        <Penguin key={penguin.id} penguin={penguin} />
+      ))}
+    </div>
+  );
 }
